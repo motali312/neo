60119142714ab72e7172ac3f5849e1ad0306327e
Shermamat <sherma.228.ss@gmail.com>
2022-01-11 19:51:10 +0600

    add less4.js

--- again.js
+++ again.js
@@ -1,30 +1,32 @@
-// const obj = {
-//     a: 10,
-//     b: true
+// // const obj = {
+// //     a: 10,
+// //     b: true
+// // }
+// // console.log(obj)
+
+// // const copyOfA = obj
+// // // copyOfA.a = 20
+// // copyOfA.c = 'abc'
+// // copyOfA.dweq = [12]
+// // console.log(obj)
+
+// // const a = () => {
+// //     console.log('Hey')
+// // }
+
+// // a() 
+// const myCity = {
+//     city: 'New',
+//     info: { 
+//         popular: true,
+//         country: 'Usa'
+//     }
 // }
-// console.log(obj)
+// // console.log(myCity.city)
+// // myCity.city = 'Bishkek'
+// // console.log(myCity.city)
+// console.log(myCity.info.popular)
+// delete myCity.info['popular']
+// console.log(myCity)
 
-// const copyOfA = obj
-// // copyOfA.a = 20
-// copyOfA.c = 'abc'
-// copyOfA.dweq = [12]
-// console.log(obj)
-
-// const a = () => {
-//     console.log('Hey')
-// }
-
-// a() 
-const myCity = {
-    city: 'New',
-    info: { 
-        popular: true,
-        country: 'Usa'
-    }
-}
-// console.log(myCity.city)
-// myCity.city = 'Bishkek'
-// console.log(myCity.city)
-console.log(myCity.info.popular)
-delete myCity.info['popular']
-console.log(myCity)
+console.log('после    ' + '    до')
